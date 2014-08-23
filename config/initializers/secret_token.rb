5d3f2e9fa4a519dc497b0c35f1b39c3b20343430
Gesang <gesarlhamo80@gmail.com>
2014-08-23 13:53:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NeueveCream::Application.config.secret_key_base = '87669429d7c359de8c87a2d39bb38010173c7076bd3dff9d597f73f4e96da7f751cac782e3d7e59936726e56b3b92c1ede1adca72c1058addabdd58c9c0626dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NeueveCream::Application.config.session_store :cookie_store, key: '_neueve_cream_session'
